40626ca10517e4fb1eeda625db4427146cf4bd13
Yakup Zengin <yakupzngin@gmail.com>
2024-02-25 19:19:57 +0300

    Config

---
+++ src/main/java/com/yakpzngin/JavaDevOpsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.yakpzngin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevOpsPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevOpsPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yakpzngin/Java045DevOpsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakpzngin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java045DevOpsPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
